7ebd3c4e110f8d327cf9f695fb69bd5ad5ac258b
jimd1989 <jamesdalrymple@protonmail.com>
2022-08-19 07:47:42 +1000

    New unfamiliar query

--- db/queries/listen-again.sql
+++ db/queries/listen-again.sql
@@ -1,23 +1,22 @@
 .headers on
 .mode html
-WITH album_plays AS (
-  SELECT COUNT(plays.date) AS 'count', plays.album
-  FROM plays 
-  GROUP BY plays.album
-),
-underplayed AS (
-  SELECT artists.name, albums.title, albums.id
-  FROM album_plays 
-  JOIN albums ON (album_plays.album = albums.id) 
-  JOIN artists ON (albums.artist = artists.id) 
-  WHERE count < 2 AND albums.title <> 'Unknown Album' 
-  ORDER BY RANDOM() 
-  LIMIT 15
+ WITH album_plays AS (
+  SELECT 
+    COUNT(plays.date) as 'count', 
+    date(plays.date, 'unixepoch', 'localtime') AS 'last',
+    artists.name,
+    albums.title
+  FROM albums 
+  LEFT JOIN plays ON (albums.id = plays.album)
+  JOIN artists ON (albums.artist = artists.id)
+  WHERE albums.title <> 'Unknown Album'
+  GROUP BY albums.id
 )
-SELECT 
-  underplayed.name AS 'Artist',
-  underplayed.title AS 'Album',
-  date(plays.date, 'unixepoch', 'localtime') AS 'Last Played'
-FROM underplayed
-JOIN plays ON (underplayed.id = plays.album)
-GROUP BY underplayed.id;
+SELECT
+  album_plays.name AS 'Artist',
+  album_plays.title AS 'Album',
+  last AS 'Last Played'
+FROM album_plays
+WHERE count < 2
+ORDER BY RANDOM()
+LIMIT 15;
