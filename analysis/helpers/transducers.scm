fbf31d81ade8ec3ba06a13d03dc57997e9649862
jimd1989 <jamesdalrymple@protonmail.com>
2026-06-17 16:28:13 +1000

    more wip

--- analysis/helpers/transducers.scm
+++ analysis/helpers/transducers.scm
@@ -9,22 +9,25 @@
                      ((acc) (r acc))
                      ((acc ω) (r acc (f ω))))))
 ; where
-;  f      = primary data transformer (a → b)
-;  reduce = function to combine results (c → b → c) not strictly monoidal
-;  acc    = previous value in composition chain (think folds)
-;  ω      = current value in composition chain (think folds)
+;  f   = primary data transformer (a → b)
+;  r   = function to combine results (c → b → c) not strictly monoidal
+;  acc = previous value in composition chain (think folds)
+;  ω   = current value in composition chain (think folds)
 ; the case-lambda (acc) is for "flushing remaining data", which isn't a problem
 ; in pure idiomatic transducers, but needs to be plumbed through towards any
 ; stateful batched transducers.
+; the niladic case-lambda () is apparently needed to conform to the cannonical
+; definition of transducers, but it seems like a convenient constructor more
+; than anything else
 
 (← inc (⊙t (λ (n) (+ n 1))))
 
 ; slightly more complex: stateful transducers can call reduce conditionally.
-; f → item/buffer transformer
-; p → buffer yield predicate
-; g → buffer yield transformer
-; h → buffer post-yield transformer
-; i → optional flush buffer yield transformer
+; f = item/buffer transformer
+; p = buffer yield predicate
+; g = buffer yield transformer
+; h = buffer post-yield transformer
+; i = optional flush buffer yield transformer
 (← (until f p g h #!optional (i g))
   (λ (r)
     (∃ ((buf ∅))
@@ -44,11 +47,24 @@
          (λ (buf) `(,(↑ buf)))
          ⊖))
 
+; more difficult: calling n child pipelines, then outputting them into a flat
+; stream. most complexity comes from proper flushing of all children.
+; needs to pass the unary case to all children to flush subordinate pipes, then
+; flatten them with the parent reducer, them emit that.
+; in both unary and dyadic cases, the result must be immediately reified to a
+; literal list before emitted into the rest of the pipeline.
+; honestly not 100% sure this shit works
+
+; possibly a more general "identity" helper: reify to list on dyadic, thing
+; itself for unary.
+(← It (λλ ((α) α) ((acc ω) (⊃ acc ω))))
+
 (← (mux . fs)
   (λ (r)
-    (∃ ((gs (∀ ((C &) ⊃) fs)))
+    (∃ ((gs (∀ ((C &) It) fs)))
       (λλ (() (r))
-          ((acc) (⇐ $$ acc gs))
+          ((acc) (∃ ((flushed (⇐ (λ (α g) (g α)) ∅ gs)))
+                   (∃ ((αs (⇐ (λ (α ω) (r α ω)) acc flushed))) (r αs))))
           ((acc ω) (∃ ((αs (⇐ (λ (α g) (g α ω)) ∅ gs))) (⇐ r acc αs)))))))
 
 ; traversal → how ωs is traversed: foldl, etc
@@ -56,14 +72,11 @@
 ; reduce    → combines all results into reified final value
 ; acc       → empty accumulator state, just like a fold
 ; ωs        → raw inputs, could be physical, could be port
-;(← (transduce traversal pipeline reduce acc ωs)
-;  (∃ ((step (λλ ((α ω) (reduce α ω)) ((α) α)))
-;      (f (pipeline step)))
-;    (f (traversal f acc ωs))))
 
-(← (transduce traversal pipeline reduce acc ωs)
+(← (transduce traversal pipeline reduce acc ωs #!optional (flush? #t))
   (∃ ((step (λλ ((α ω) (reduce α ω)) ((α) α)))
       (f (pipeline step)))
-    (traversal f acc ωs)))
+     (∃ ((res (traversal f acc ωs)))
+       (? flush? (f res) res))))
 
-(transduce ⇐ (∘ inc (mux (⊙t (K 100)) inc) (chunk 2))  ⊃ ∅ (list 1 2 3))
+(transduce ⇐ (∘ inc (mux (⊙t (K 100)) inc) inc (chunk 4))  ⊃ ∅ (list 1 2 3) #t)
